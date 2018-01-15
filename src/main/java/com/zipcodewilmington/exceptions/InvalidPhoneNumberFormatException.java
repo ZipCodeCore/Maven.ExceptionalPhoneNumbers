037a4a1cab60a17aeb97a2cf219ca499b2d59618
Leon <leon@Leons-MacBook-Pro.local>
2018-01-15 15:07:11 -0500

    updated `InvalidPhoneNumberFormatException` class

--- src/main/java/com/zipcodewilmington/exceptions/InvalidPhoneNumberFormatException.java
+++ src/main/java/com/zipcodewilmington/exceptions/InvalidPhoneNumberFormatException.java
@@ -1,9 +1,7 @@
 package com.zipcodewilmington.exceptions;
 
-import java.io.IOException;
-
 /**
  * Created by leon on 5/10/17.
  */ // Checked Exception
-public final class InvalidPhoneNumberFormatException extends IOException {
+public final class InvalidPhoneNumberFormatException extends Exception {
 }
