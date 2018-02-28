2f7aab71b3c8cf380aec8b199d5f86a07081b3ee
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-02-28 14:03:13 -0500

    Create phone number good, create safely experimenting

--- src/main/java/com/zipcodewilmington/phone/PhoneNumberFactory.java
+++ src/main/java/com/zipcodewilmington/phone/PhoneNumberFactory.java
@@ -26,7 +26,7 @@ public final class PhoneNumberFactory {
     /**
      * @return an instance of PhoneNumber with randomly generated phone number value
      */ //TODO - Implement logic
-    public static PhoneNumber createRandomPhoneNumber() {
+    public static PhoneNumber createRandomPhoneNumber() throws InvalidPhoneNumberFormatException {
         return createPhoneNumberSafely(-1, -1, -1);
     }
 
@@ -37,8 +37,16 @@ public final class PhoneNumberFactory {
      * @param phoneLineCode     - 4 digit code
      * @return a new phone number object
      */ //TODO - if input is valid, return respective PhoneNumber object, else return null
-    public static PhoneNumber createPhoneNumberSafely(int areaCode, int centralOfficeCode, int phoneLineCode) {
-        return createPhoneNumber(null);
+    public static PhoneNumber createPhoneNumberSafely(int areaCode, int centralOfficeCode, int phoneLineCode) throws InvalidPhoneNumberFormatException{
+        try {
+            // if (3 == String.valueOf(areaCode).length() && 3 == String.valueOf(centralOfficeCode).length() && 4 == String.valueOf(phoneLineCode).length()) {
+            String tempString = String.valueOf(areaCode) + String.valueOf(centralOfficeCode) + String.valueOf(phoneLineCode);
+            return createPhoneNumber(tempString);
+            //     return createPhoneNumber(tempString);
+            // }
+        } catch (InvalidPhoneNumberFormatException inv) {
+            return null;
+        }
     }
 
     /**
@@ -46,7 +54,7 @@ public final class PhoneNumberFactory {
      * @return a new phone number object
      * @throws InvalidPhoneNumberFormatException - thrown if phoneNumberString does not match acceptable format
      */ // TODO - Add throws statement to method signature
-    public static PhoneNumber createPhoneNumber(String phoneNumberString) {
-        return null;
+    public static PhoneNumber createPhoneNumber (String phoneNumberString) throws InvalidPhoneNumberFormatException {
+        return new PhoneNumber(phoneNumberString);
     }
 }
