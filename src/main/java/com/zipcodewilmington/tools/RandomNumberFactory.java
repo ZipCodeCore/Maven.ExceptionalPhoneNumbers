93e5ea930d60743c791aac8b052fa7bc97c64c5f
Amy Gill <amygill@MacBook-Pro-2.local>
2018-02-28 20:19:17 -0500

    all test passing

--- src/main/java/com/zipcodewilmington/tools/RandomNumberFactory.java
+++ src/main/java/com/zipcodewilmington/tools/RandomNumberFactory.java
@@ -11,11 +11,13 @@ public abstract class RandomNumberFactory {
 
     /** @return a random float between the specified min and max numeric range */
     public static Float createFloat(float min, float max) {
+
         return random.nextFloat() * (max - min) + min;
     }
 
     /** @return a random integer between the specified min and max numeric range */
     public static Integer createInteger(Integer min, Integer max) {
+
         return createFloat(min, max).intValue();
     }
 }
