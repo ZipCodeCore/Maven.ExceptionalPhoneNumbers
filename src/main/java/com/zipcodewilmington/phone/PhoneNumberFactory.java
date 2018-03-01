83823c1ca6c7c8115f276323798dd65c298f1295
vvmk <vincentmasiello@gmail.com>
2018-02-28 21:00:48 -0500

    Handle exceptions, prevent errors, save the planet

--- src/main/java/com/zipcodewilmington/phone/PhoneNumberFactory.java
+++ src/main/java/com/zipcodewilmington/phone/PhoneNumberFactory.java
@@ -2,6 +2,7 @@ package com.zipcodewilmington.phone;
 
 import com.zipcodewilmington.exceptions.InvalidPhoneNumberFormatException;
 
+import java.util.Random;
 import java.util.logging.Logger;
 
 /**
@@ -20,16 +21,34 @@ public final class PhoneNumberFactory {
      * @return array of randomly generated PhoneNumber objects
      */ //TODO - Implement logic
     public static PhoneNumber[] createRandomPhoneNumberArray(int phoneNumberCount) {
-        return null;
+        PhoneNumber[] ret = new PhoneNumber[phoneNumberCount];
+        for (int i=0;i<ret.length;i++)
+            ret[i] = createRandomPhoneNumber();
+        return ret;
     }
 
     /**
      * @return an instance of PhoneNumber with randomly generated phone number value
      */ //TODO - Implement logic
     public static PhoneNumber createRandomPhoneNumber() {
-        return createPhoneNumberSafely(-1, -1, -1);
+        return createPhoneNumberSafely(randAreaOrOfficeCode(), randAreaOrOfficeCode(), randLineCode());
     }
 
+    static int randAreaOrOfficeCode() {
+        return genRandomSequence(3);
+    }
+
+    static int randLineCode() {
+        return genRandomSequence(4);
+    }
+
+    static int genRandomSequence(int howManyDigits) {
+        Random r = new Random();
+        int sum = 0;
+        for (int i=0;i<howManyDigits;i++)
+            sum += r.nextInt(10) * Math.pow(10, i);
+        return sum;
+    }
 
     /**
      * @param areaCode          - 3 digit code
@@ -38,7 +57,14 @@ public final class PhoneNumberFactory {
      * @return a new phone number object
      */ //TODO - if input is valid, return respective PhoneNumber object, else return null
     public static PhoneNumber createPhoneNumberSafely(int areaCode, int centralOfficeCode, int phoneLineCode) {
-        return createPhoneNumber(null);
+        String phoneNumberString = String.format("(%03d)-%03d-%04d", areaCode, centralOfficeCode, phoneLineCode);
+        try {
+            logger.info("Attempting to create a new PhoneNumber object with a value of " + phoneNumberString);
+            return createPhoneNumber(phoneNumberString);
+        } catch (InvalidPhoneNumberFormatException ipne) {
+            logger.info(phoneNumberString + "  is not a valid phone number");
+            return null;
+        }
     }
 
     /**
@@ -46,7 +72,7 @@ public final class PhoneNumberFactory {
      * @return a new phone number object
      * @throws InvalidPhoneNumberFormatException - thrown if phoneNumberString does not match acceptable format
      */ // TODO - Add throws statement to method signature
-    public static PhoneNumber createPhoneNumber(String phoneNumberString) {
-        return null;
+    public static PhoneNumber createPhoneNumber(String phoneNumberString) throws InvalidPhoneNumberFormatException{
+        return new PhoneNumber(phoneNumberString);
     }
 }
--- src/main/java/com/zipcodewilmington/tools/RandomNumberFactory.java
+++ src/main/java/com/zipcodewilmington/tools/RandomNumberFactory.java
@@ -1,6 +1,6 @@
 package com.zipcodewilmington.tools;
-import java.awt.*;
-import java.util.*;
+
+import java.util.Random;
 
 /**
  * Created by Leon on 2/4/2017.
@@ -9,12 +9,16 @@ import java.util.*;
 public abstract class RandomNumberFactory {
     private static final Random random = new Random();
 
-    /** @return a random float between the specified min and max numeric range */
+    /**
+     * @return a random float between the specified min and max numeric range
+     */
     public static Float createFloat(float min, float max) {
         return random.nextFloat() * (max - min) + min;
     }
 
-    /** @return a random integer between the specified min and max numeric range */
+    /**
+     * @return a random integer between the specified min and max numeric range
+     */
     public static Integer createInteger(Integer min, Integer max) {
         return createFloat(min, max).intValue();
     }
