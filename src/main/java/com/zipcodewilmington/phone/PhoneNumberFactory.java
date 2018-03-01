baafa0140000ca794d4a8ade6fd8fdf631c8fb39
Keith Brinker <keithbrinker@zipcoders-MacBook-Pro-2.local>
2018-03-01 15:26:40 -0500

    finished

--- src/main/java/com/zipcodewilmington/phone/PhoneNumberFactory.java
+++ src/main/java/com/zipcodewilmington/phone/PhoneNumberFactory.java
@@ -1,6 +1,7 @@
 package com.zipcodewilmington.phone;
 
 import com.zipcodewilmington.exceptions.InvalidPhoneNumberFormatException;
+import com.zipcodewilmington.tools.RandomNumberFactory;
 
 import java.util.logging.Logger;
 
@@ -20,15 +21,28 @@ public final class PhoneNumberFactory {
      * @return array of randomly generated PhoneNumber objects
      */ //TODO - Implement logic
     public static PhoneNumber[] createRandomPhoneNumberArray(int phoneNumberCount) {
-        return null;
+        PhoneNumber[] phoneNumberArray = new PhoneNumber[phoneNumberCount];
+        for (int i = 0; i < phoneNumberCount; i++){
+            phoneNumberArray[i]=createRandomPhoneNumber();
+        }
+        return phoneNumberArray;
+
     }
 
     /**
      * @return an instance of PhoneNumber with randomly generated phone number value
      */ //TODO - Implement logic
     public static PhoneNumber createRandomPhoneNumber() {
-        return createPhoneNumberSafely(-1, -1, -1);
+        int areaCode = RandomNumberFactory.createInteger(100, 999);
+        int centralOfficeCode = RandomNumberFactory.createInteger(100, 999);
+        int phoneLineCode = RandomNumberFactory.createInteger(1000, 9999);
+
+        return createPhoneNumberSafely(areaCode, centralOfficeCode, phoneLineCode);
     }
+
+
+
+
 
 
     /**
@@ -38,7 +52,18 @@ public final class PhoneNumberFactory {
      * @return a new phone number object
      */ //TODO - if input is valid, return respective PhoneNumber object, else return null
     public static PhoneNumber createPhoneNumberSafely(int areaCode, int centralOfficeCode, int phoneLineCode) {
-        return createPhoneNumber(null);
+        StringBuilder builder = new StringBuilder();
+        builder.append("("+areaCode +")"+ "-"+ centralOfficeCode + "-"+phoneLineCode);
+        String phoneNumberString = builder.toString();
+        try{
+            return createPhoneNumber(phoneNumberString);
+        }catch(InvalidPhoneNumberFormatException e){
+            logger.info(phoneNumberString + " is incorrect, please try again.");
+            return null;
+
+
+        }
+
     }
 
     /**
@@ -46,7 +71,7 @@ public final class PhoneNumberFactory {
      * @return a new phone number object
      * @throws InvalidPhoneNumberFormatException - thrown if phoneNumberString does not match acceptable format
      */ // TODO - Add throws statement to method signature
-    public static PhoneNumber createPhoneNumber(String phoneNumberString) {
-        return null;
+    public static PhoneNumber createPhoneNumber(String phoneNumberString) throws InvalidPhoneNumberFormatException {
+        return new PhoneNumber(phoneNumberString);
     }
 }
