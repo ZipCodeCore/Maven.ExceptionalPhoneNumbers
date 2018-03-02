566a944cc21e767dab4d2502b55a9c9bebe791a0
ahsonali <ahsonac@gmail.com>
2018-03-02 08:42:53 -0500

    done, off to the farmland

--- src/main/java/com/zipcodewilmington/phone/PhoneNumberFactory.java
+++ src/main/java/com/zipcodewilmington/phone/PhoneNumberFactory.java
@@ -1,13 +1,15 @@
 package com.zipcodewilmington.phone;
 
 import com.zipcodewilmington.exceptions.InvalidPhoneNumberFormatException;
+import com.zipcodewilmington.tools.RandomNumberFactory;
+
 
 import java.util.logging.Logger;
-
 /**
  * Created by leon on 5/1/17.
  */
-public final class PhoneNumberFactory {
+public final class PhoneNumberFactory
+{
     private static final Logger logger = Logger.getGlobal();
 
     private PhoneNumberFactory() {
@@ -19,15 +21,28 @@ public final class PhoneNumberFactory {
      * @param phoneNumberCount - number of PhoneNumber objects to instantiate
      * @return array of randomly generated PhoneNumber objects
      */ //TODO - Implement logic
-    public static PhoneNumber[] createRandomPhoneNumberArray(int phoneNumberCount) {
-        return null;
+    public static PhoneNumber[] createRandomPhoneNumberArray(int phoneNumberCount)
+    {
+        //Initializing an array with the size of phoneNumberCount
+        PhoneNumber [] phoneNumberArray = new PhoneNumber[phoneNumberCount];
+
+        for(int i =0; i<phoneNumberCount; i++)
+        {
+            phoneNumberArray[i]=createRandomPhoneNumber();
+        }
+        return phoneNumberArray;
     }
 
     /**
      * @return an instance of PhoneNumber with randomly generated phone number value
      */ //TODO - Implement logic
-    public static PhoneNumber createRandomPhoneNumber() {
-        return createPhoneNumberSafely(-1, -1, -1);
+    public static PhoneNumber createRandomPhoneNumber()
+    {
+        int areaCode = RandomNumberFactory.createInteger(100,999);
+        int centralOfficeCode = RandomNumberFactory.createInteger(100,999);
+        int phoneLineCode = RandomNumberFactory.createInteger(1000,9999);
+
+        return createPhoneNumberSafely(areaCode, centralOfficeCode, phoneLineCode);
     }
 
 
@@ -37,8 +52,26 @@ public final class PhoneNumberFactory {
      * @param phoneLineCode     - 4 digit code
      * @return a new phone number object
      */ //TODO - if input is valid, return respective PhoneNumber object, else return null
-    public static PhoneNumber createPhoneNumberSafely(int areaCode, int centralOfficeCode, int phoneLineCode) {
-        return createPhoneNumber(null);
+    public static PhoneNumber createPhoneNumberSafely(int areaCode, int centralOfficeCode, int phoneLineCode)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("("+areaCode+")" + "-" + centralOfficeCode + "-" + phoneLineCode);
+        String phoneNumberString = sb.toString();
+
+        try
+        {
+            return createPhoneNumber(phoneNumberString);
+        }
+
+        catch (InvalidPhoneNumberFormatException e)
+        {
+            logger.info(phoneNumberString + " is not a valid phone number entry");
+            return null;
+        }
+
+
+
+
     }
 
     /**
@@ -46,7 +79,9 @@ public final class PhoneNumberFactory {
      * @return a new phone number object
      * @throws InvalidPhoneNumberFormatException - thrown if phoneNumberString does not match acceptable format
      */ // TODO - Add throws statement to method signature
-    public static PhoneNumber createPhoneNumber(String phoneNumberString) {
-        return null;
+    public static PhoneNumber createPhoneNumber(String phoneNumberString) throws InvalidPhoneNumberFormatException
+    {
+        //logger.info("Attempting to create a new PhoneNumber object with a value of " + phoneNumberString);
+        return new PhoneNumber(phoneNumberString);
     }
 }
--- src/main/java/com/zipcodewilmington/tools/RandomNumberFactory.java
+++ src/main/java/com/zipcodewilmington/tools/RandomNumberFactory.java
@@ -10,12 +10,14 @@ public abstract class RandomNumberFactory {
     private static final Random random = new Random();
 
     /** @return a random float between the specified min and max numeric range */
-    public static Float createFloat(float min, float max) {
+    public static Float createFloat(float min, float max)
+    {
         return random.nextFloat() * (max - min) + min;
     }
 
     /** @return a random integer between the specified min and max numeric range */
-    public static Integer createInteger(Integer min, Integer max) {
+    public static Integer createInteger(Integer min, Integer max)
+    {
         return createFloat(min, max).intValue();
     }
 }
