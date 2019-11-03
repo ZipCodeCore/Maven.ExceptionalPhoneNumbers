9f6a8264e8bf36b9fa94a14a69a212c6dd80165b
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-03 16:13:33 -0500

    Add logic for create phone number and create phone number safely

--- src/main/java/com/zipcodewilmington/phone/PhoneNumberFactory.java
+++ src/main/java/com/zipcodewilmington/phone/PhoneNumberFactory.java
@@ -26,7 +26,7 @@ public final class PhoneNumberFactory {
     /**
      * @return an instance of PhoneNumber with randomly generated phone number value
      */ //TODO - Implement logic
-    public static PhoneNumber createRandomPhoneNumber() {
+    public static PhoneNumber createRandomPhoneNumber() throws InvalidPhoneNumberFormatException {
         return createPhoneNumberSafely(-1, -1, -1);
     }
 
@@ -37,8 +37,17 @@ public final class PhoneNumberFactory {
      * @param phoneLineCode     - 4 digit code
      * @return a new phone number object
      */ //TODO - if input is valid, return respective PhoneNumber object, else return null
-    public static PhoneNumber createPhoneNumberSafely(int areaCode, int centralOfficeCode, int phoneLineCode) {
-        return createPhoneNumber(null);
+    public static PhoneNumber createPhoneNumberSafely(int areaCode, int centralOfficeCode, int phoneLineCode) throws InvalidPhoneNumberFormatException {
+        String newPhoneNumber = "";
+
+        newPhoneNumber = "(" + areaCode + ")-" + centralOfficeCode + "-" + phoneLineCode;
+
+        try {
+            return  createPhoneNumber(newPhoneNumber);
+        } catch (InvalidPhoneNumberFormatException e){
+            return null;
+        }
+
     }
 
     /**
@@ -46,7 +55,10 @@ public final class PhoneNumberFactory {
      * @return a new phone number object
      * @throws InvalidPhoneNumberFormatException - thrown if phoneNumberString does not match acceptable format
      */ // TODO - Add throws statement to method signature
-    public static PhoneNumber createPhoneNumber(String phoneNumberString) {
-        return null;
+    public static PhoneNumber createPhoneNumber(String phoneNumberString) throws InvalidPhoneNumberFormatException {
+
+        PhoneNumber newPhoneNumber = new PhoneNumber(phoneNumberString);
+
+        return newPhoneNumber;
     }
 }
